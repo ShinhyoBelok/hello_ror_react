3af7c8c0b0b18e1cc5cf78489f257f986b03ee5e
ShinhyoBelok <shibo6937@gmail.com>
2023-05-03 16:05:45 -0400

    Add react and redux

--- app/controllers/api_greetings_controller.rb
+++ app/controllers/api_greetings_controller.rb
@@ -1,6 +1,6 @@
 class ApiGreetingsController < ApplicationController
     def random
         @greeting = Greeting.order("RANDOM()").pluck(:message).first
-        render json: { message: @greeting }
+        render json: @greeting
     end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,12 +3,16 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function App() {
-  return (<h1>Hello World!</h1>);
-}
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Greeter from './components/Greeter';
+import store from './redux/configureStore';
 
 ReactDOM.render(
-  <App/>,
+  <Provider store={store}>
+    <BrowserRouter>
+        <Greeter />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
